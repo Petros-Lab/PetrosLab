130350a3a3b340ebac70bc9df0efc89fee7fe683
Christopher Rhodes <ctrhodes@users.noreply.github.com>
2018-11-19 13:42:57 -0500

    Update start_biowulf_node.sh

--- start_biowulf_node.sh
+++ start_biowulf_node.sh
@@ -3,6 +3,7 @@
 if [[ ":$PATH:" == *":$HOME/bin:"* ]]; then
 	echo "Your path is correctly set"
 else
-	echo 'export PATH=$PATH:$HOME/run' >> ~/.bashrc
+	echo 'export PATH=$PATH:$HOME/run' >> ~/.bash_profile
+	source ~/.bash_profile
 	echo "Your path was missing ~/bin, it has been added to your .bash_profile file."
 fi
