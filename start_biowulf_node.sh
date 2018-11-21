7fc653cc86dccb52e456a13518d2dcdbdb5d7ec9
Christopher Rhodes <ctrhodes@users.noreply.github.com>
2018-11-21 13:42:46 -0500

    Update start_biowulf_node.sh

--- start_biowulf_node.sh
+++ start_biowulf_node.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# makes an executable dir for scripts if does not exist
+mkdir -p $HOME/bin
+
+# add executable dir to path if not 
 if [[ ":$PATH:" == *":$HOME/bin:"* ]]; then
-	echo "Your path is correctly set"
+echo "Your path is correctly set"
 else
-	echo 'export PATH=$PATH:$HOME/run' >> ~/.bash_profile
-	source ~/.bash_profile
-	echo "Your path was missing ~/bin, it has been added to your .bash_profile file."
+echo 'export PATH=$PATH:$HOME/bin' >> $HOME/.bash_profile
+source ~/.bash_profile
+echo "Your path was missing ~/bin, it has been added to your .bash_profile file."
 fi
+
